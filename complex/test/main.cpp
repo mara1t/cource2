d9cfc70db15d9d4804d36c27b4fd9492a54e8124
Зиятдинов Марат <zmarat30122002@mail.ru>
2022-09-24 09:34:47 +0300

    deleted template

--- complex/test/main.cpp
+++ complex/test/main.cpp
@@ -1,16 +1,17 @@
 #include "..//include//complex.h"
 
+
 int main()
 {
-    complex::complex_num<double> tmp_num1{10, 20};
+    complex::complex_num tmp_num1{10, 20};
     std::cout << "first num = ";
     tmp_num1.print_num();
 
-    complex::complex_num<double> tmp_num2{10, 20};
+    complex::complex_num tmp_num2{10, 20};
     std::cout << "second num = ";
     tmp_num2.print_num();
     
-    complex::complex_num<double> tmp_num3;
+    complex::complex_num tmp_num3;
 
     tmp_num3 = tmp_num2 + tmp_num1;
     std::cout << "sum = ";
